88c2ffcdae9405c7766d7d117a2bd97694d3e901
cc <cc123456>
2021-04-25 18:46:02 +0800

    添加代码拷贝功能

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -6,7 +6,7 @@
 *   @link: https://github.com/CaiJimmy/hugo-theme-stack
 */
 import StackGallery from "ts/gallery";
-import { getColor } from 'ts/color';
+import {getColor} from 'ts/color';
 import menu from 'ts/menu';
 import createElement from 'ts/createElement';
 import StackColorScheme from 'ts/colorScheme';
@@ -69,6 +69,48 @@ declare global {
         createElement: any;
         Stack: any
     }
+}
+
+//拷贝代码按钮
+var highlightBlocks = document.getElementsByClassName('highlight');
+if (highlightBlocks != null) {
+    console.info(highlightBlocks);
+    Array.prototype.forEach.call(highlightBlocks, addCopyButton);
+}
+function selectText(node) {
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(node);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    return selection;
+}
+
+function flashCopyMessage(el, msg) {
+    el.textContent = msg;
+    setTimeout(function () {
+        el.textContent = "Copy";
+    }, 1000);
+}
+
+function addCopyButton(containerEl) {
+    var copyBtn = document.createElement("button");
+    copyBtn.className = "highlight-copy-btn";
+    copyBtn.textContent = "Copy";
+
+    var codeEl = containerEl.firstElementChild.firstElementChild.firstElementChild.firstElementChild.children[1];
+    copyBtn.addEventListener('click', function () {
+        try {
+            var selection = selectText(codeEl);
+            document.execCommand('copy');
+            selection.removeAllRanges();
+            flashCopyMessage(copyBtn, 'Copied!')
+        } catch (e) {
+            console && console.log(e);
+            flashCopyMessage(copyBtn, 'Failed :\'(')
+        }
+    });
+    containerEl.appendChild(copyBtn);
 }
 
 window.Stack = Stack;
---
+++ content/post/Idea快捷键/index.md
@@ -0,0 +1,33 @@
++++
+author = "CC"
+title = "IDEA快捷键For Windows"
+date = "2021-04-21"
+description = ""
+categories = [
+"idea",
+"java"
+]
+tags = [
+]
+image = ""
+tableofcontents = false
++++
+
+## IDEA原始PDF文档查看 ##
+点击Idea顶部菜单栏中 **<font color='red'>Help</font>** -> **<font color='red'>Keymap Reference</font>**，后出现一个快捷键PDF文档。
+
+### 一、Editing编辑 ###
+ **代码补全**
+|  快捷键   | 说明  | 使用频率  |
+|  ----  | ---- | ----  |
+| Ctrl + Space  | 基本的代码补全（类名、方法、变量） | ★  |
+| Ctrl + Shift + Space  | 代码智能补全（类名、方法、变量） | ☆  |
+| Ctrl + Shift + Enter  | 智能结束代码 | ☆  |
+
+**显示查看**
+|  快捷键   | 说明  | 使用频率  |
+|  ----  | ---- | ----  |
+| Ctrl + P  | 显示该方法的参数信息（光标放在被调用方法的圆括号内，然后按此快捷键） | ★★★★  |
+| Ctrl + Q  | 代码智能补全（类名、方法、变量） | ☆  |
+| Ctrl + Shift + Enter  | 智能结束代码 | ☆  |
+
